7ed5ad183e0dc88b5c92c830a9f296ef8740e5e4
Victus <saverio041@outlook.it>
2025-06-05 10:22:33 +0200

    Miglioramento della responsività

--- src/main/webapp/js/dettagliArticolo.js
+++ src/main/webapp/js/dettagliArticolo.js
@@ -71,6 +71,8 @@ function modificaRecensione(idRecensione) {
 
     card.replaceWith(newCard);
 }
+
+
 async function checkForm(idRecensione) {
     let stelleInput = document.getElementById("voto");
     let testoInput = document.getElementById("testo");
